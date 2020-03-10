24ad97758ed024df55a261c9de8097a58aad7c61
Eric Zhao <21zhaoe@protonmail.com>
2020-03-10 11:21:44 -0400

    qutebrowser: Download userscripts at qutebrowser boot

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,9 +1,30 @@
 import sys
 import os
+import pathlib
+import urllib.request
 
 # pylint: disable=C0111
 c = c  # noqa: F821 pylint: disable=E0602,C0103
 config = config  # noqa: F821 pylint: disable=E0602,C0103
+
+# download greasemonkey scripts
+src_path = os.path.dirname(os.path.abspath(__file__))
+greasemonkey_path = os.path.join(src_path, "greasemonkey")
+pathlib.Path(greasemonkey_path).mkdir(parents=True, exist_ok=True)
+with open(os.path.join(src_path, "greasemonkey-list")) as f:
+    script_urls = [line.rstrip('\n') for line in f]
+for url in script_urls:
+    split = url.split(",", 1)
+
+    print(split)
+    url = split[0].strip()
+    filename = split[1].strip()
+    file = pathlib.Path(os.path.join(greasemonkey_path, filename))
+    if file.is_file() is False:
+        with urllib.request.urlopen(url) as response, \
+                open(file, 'wb') as out_file:
+            data = response.read()
+            out_file.write(data)
 
 # load autoconfig.yml
 config.load_autoconfig()
