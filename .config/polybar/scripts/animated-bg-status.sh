b09e4133c187a5d6459af5eb7a9d3ddfd9a612a7
Eric Zhao <21zhaoe@protonmail.com>
2020-08-07 22:00:43 -0400

    polybar: Check for wallpaper toggle file before reading

--- .config/polybar/scripts/animated-bg-status.sh
+++ .config/polybar/scripts/animated-bg-status.sh
@@ -4,9 +4,13 @@ cache_dir="$XDG_CACHE_HOME/wallpaper"
 pid_file="$cache_dir/animated_pid"
 toggle_file="$cache_dir/toggled"
 
-read -d $'\0x4' toggle < "$toggle_file"
-if [[ $toggle == 1 ]]; then
-  echo ""
+if [[ -f "$toggle_file" ]]; then
+  read -d $'\0x4' toggle <"$toggle_file"
+  if [[ $toggle == 1 ]]; then
+    echo ""
+  else
+    echo ""
+  fi
 else
   echo ""
 fi
