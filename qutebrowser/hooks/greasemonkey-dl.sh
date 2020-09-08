ea437d1acf22aed8db51c6fee921a1fc1b501914
Eric Zhao <21zhaoe@protonmail.com>
2020-09-07 21:17:51 -0400

    qutebrowser: Some github userscripts

--- qutebrowser/hooks/greasemonkey-dl.sh
+++ qutebrowser/hooks/greasemonkey-dl.sh
@@ -27,3 +27,33 @@ dl "oneechan.user.js" \
 dl "mouseover-image-viewer.user.js" \
   "https://greasyfork.org/scripts/404-mouseover-popup-image-viewer/code/Mouseover%20Popup%20Image%20Viewer.user.js" \
   "dd5dcd198ba84167d8e1390061df91ec4b3ea7a14828618a1caf510d4dec1a0f"
+dl "github-code-colors.user.js" \
+  "https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/github-code-colors.user.js" \
+  "98f8a6167d19480a3c001da7008f46f9fa17570068f6099308be0e5859b82508"
+dl "github-copy-code-snippet.user.js" \
+  "https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/github-copy-code-snippet.user.js" \
+  "44fd76d7ee8d9126344b1eafb4a6aace7df4d5b5abc0e6e340d47a0e1563a652"
+dl "github-issue-counts.user.js" \
+  "https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/github-issue-counts.user.js" \
+  "0826b1c993eaf6d87021d8f32c33c3061d673610c6ee18bc202e33faa5ba055f"
+dl "github-issue-comments.user.js" \
+  "https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/github-issue-comments.user.js" \
+  "94ed2c9c87caf11ad80fd2f95f8f5803d7d96b384599850693c773f7f071be93"
+dl "github-mentioned-links.user.js" \
+  "https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/github-mentioned-links.user.js" \
+  "ed9e97a86c49e8e25062ac0cf09ed9be103b3b0579e082a92683b0e2c8abada3"
+dl "github-label-color-picker.user.js" \
+  "https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/github-label-color-picker.user.js" \
+  "ec964e519d24ea3e4f52fe188bf1b1ce34639c7e3e067a40440ab52969a76468"
+dl "github-sort-content.user.js" \
+  "https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/github-sort-content.user.js" \
+  "1b099a3bb6f989aa2de9d16df27f025b5f9a027d5d52da6b4764352f1b537ec6"
+dl "gist-raw-links.user.js" \
+  "https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/gist-raw-links.user.js" \
+  "34684058884f4563291210f2a2d8a7ed2ebb7912b9ec84c724683d4185f3260c"
+dl "github-json-dependencies-linker.user.js" \
+  "https://raw.githubusercontent.com/jerone/UserScripts/master/Github_JSON_Dependencies_Linker/Github_JSON_Dependencies_Linker.user.js" \
+  "13ad2096156d9780ef092eeecab77eb4515cf1fd0e42de01f564cb92f6bc4a5a"
+dl "github-image-viewer.user.js" \
+  "https://raw.githubusercontent.com/jerone/UserScripts/master/Github_Image_Viewer/Github_Image_Viewer.user.js" \
+  "a830d077c4bc84f1153f77c8078f6ac868ffdb40b8467aeee0a7cc799c9ce4cf"
