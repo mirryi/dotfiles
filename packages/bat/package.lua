effc0af98fdb959ed46c8876e15ba0d9dded3626
Eric Zhao <21zhaoe@protonmail.com>
2020-11-17 23:11:31 -0500

    pkg/bat(t): Hbs templating

--- packages/bat/package.lua
+++ packages/bat/package.lua
@@ -2,11 +2,11 @@ require('lib')
 
 pkg.name = 'bat'
 
-pkg.files.trees:front().ignore:push('**/*.tmpl')
+pkg.files.trees:front().ignore:push('**/*.hbs')
 pkg.files.templates:push({
-    src = 'tree/.config/bat/config.tmpl',
+    src = 'tree/.config/bat/config.hbs',
     dest = '.config/bat/config',
-    engine = 'gotmpl'
+    engine = 'handlebars'
 })
 
 local profile = require('profile').bat
