3ee6edf8bb6ee499472d81b17e543a08e9b93b65
Eric Zhao <21zhaoe@protonmail.com>
2020-11-20 23:42:52 -0500

    pkg/nvim(t): Misc lua fix

--- packages/nvim/tree/.config/nvim/lua/treesitter.lua
+++ packages/nvim/tree/.config/nvim/lua/treesitter.lua
@@ -1,20 +1,18 @@
 local treesitter = require('nvim-treesitter.configs')
 local colorizer = require('colorizer')
 
-if not(treesitter == nil) then
-  treesitter.setup {
-    ensure_installed = 'maintained',
-    highlight = {
-      enable = true,
-      disable = { 'rust', 'zsh' },
-    },
-  }
+if not (treesitter == nil) then
+    treesitter.setup {
+        ensure_installed = 'maintained',
+        highlight = {enable = true, disable = {'rust', 'zsh'}}
+    }
 end
 
-if not(colorizer == nil) then
-  colorizer.setup{
-    '*';
-    css = { rgb_fn = true; };
-    html = { names = false; }
-  }
+if not (colorizer == nil) then
+    colorizer.setup {
+        '*',
+        css = {rgb_fn = true},
+        html = {names = false},
+        pandoc = {names = false}
+    }
 end
