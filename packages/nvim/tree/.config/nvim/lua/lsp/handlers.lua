e7ff5f2db0c6cd0f9ac36f765a086b554e5a3504
Eric Zhao <21zhaoe@protonmail.com>
2020-12-13 00:07:19 -0500

    pkg/nvim(t): Reorganize some keybindings

--- packages/nvim/tree/.config/nvim/lua/lsp/handlers.lua
+++ packages/nvim/tree/.config/nvim/lua/lsp/handlers.lua
@@ -1,6 +1,12 @@
 local completion = require('completion')
 local lsp_status = require('lsp-status')
 
+local map = function(mode, key, result, noremap)
+    if noremap == nil then noremap = true end
+    vim.api.nvim_buf_set_keymap(0, mode, key, result,
+                                {noremap = noremap, silent = true})
+end
+
 -- diagnostics handle
 vim.lsp.handlers['textDocument/publishDiagnostics'] =
     vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
@@ -33,7 +39,26 @@ local on_attach = function(client, bufnr)
         vim.api.nvim_command [[augroup END]]
     end
 
-    completion.on_attach(client, bufnr)
+    if client.resolved_capabilities.goto_definition then
+        map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+    end
+
+    if client.resolved_capabilities.completion then
+        completion.on_attach(client, bufnr)
+    end
+
+    if client.resolved_capabilities.hover then
+        map("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>")
+    end
+
+    if client.resolved_capabilities.find_references then
+        map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+    end
+
+    if client.resolved_capabilities.rename then
+        map("n", "gR", "<cmd>lua vim.lsp.buf.rename()<CR>")
+    end
+
     lsp_status.on_attach(client, bufnr)
 end
 
