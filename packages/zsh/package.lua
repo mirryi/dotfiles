21b7d0a15572dd2a09e72902c36f97ff98dc4ccd
Eric Zhao <21zhaoe@protonmail.com>
2020-12-17 11:06:41 -0500

    pkg/zsh(c): Fix local package linking

--- packages/zsh/package.lua
+++ packages/zsh/package.lua
@@ -4,8 +4,18 @@ local lfs = require('lfs')
 pkg.name = 'zsh'
 pkg.dependencies:extend('../sh')
 
-if lfs.attributes('local.zsh') then
-    pkg.files.extra:extend({src = 'local.zsh', dest = '.config/zsh/load/local'})
+local localpkg = 'local.zsh'
+if lfs.attributes(localpkg) then
+    for file in lfs.dir(localpkg) do
+        if file ~= '.' and file ~= '..' then
+            local f = localpkg .. '/' .. file
+            local attr = lfs.attributes(f)
+            pkg.files.extra:extend({
+                src = f,
+                dest = '.config/zsh/load/local/' .. file
+            })
+        end
+    end
 end
 
 -- Load local file if it exists
