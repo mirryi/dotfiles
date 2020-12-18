e07f6d2ae54d2081af6ac768a9ad4fba18ea0dd4
Eric Zhao <21zhaoe@protonmail.com>
2020-12-17 21:53:18 -0500

    pkg/sh(t): Use exa and bat

--- packages/fzf/package.lua
+++ packages/fzf/package.lua
@@ -6,6 +6,6 @@ pkg.dependencies:extend('../sh')
 local profile = require('profile').fzf
 pkg.files.extra:push({
     src = 'themes/' .. profile.name,
-    dest = '.config/sh/envc/fzf'
+    dest = '.config/sh/load/fzf/theme'
 })
 
