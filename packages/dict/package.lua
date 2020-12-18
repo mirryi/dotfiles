4002b68da0fa2cb95489715ab89b59fd8ed300a4
Eric Zhao <21zhaoe@protonmail.com>
2020-12-18 16:10:00 -0500

    pkg: Various reorganization

--- packages/dict/package.lua
+++ packages/dict/package.lua
@@ -1,7 +1,7 @@
 require('lib')
 
-pkg.name = 'yarn'
-pkg.dependencies:extend('../../sh')
+pkg.name = 'dict'
+pkg.dependencies:extend('../sh')
 
 -- Load local file if it exists
 require_opt('local')
