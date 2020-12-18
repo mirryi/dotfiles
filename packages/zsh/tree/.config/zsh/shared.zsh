63b2a591839ed86bf1b22dfa831576e76302b5c6
Eric Zhao <21zhaoe@protonmail.com>
2020-12-18 10:09:47 -0500

    pkg/zsh(t): Fix env.zsh

--- packages/zsh/tree/.config/zsh/shared.zsh
+++ packages/zsh/tree/.config/zsh/shared.zsh
@@ -1,15 +1,15 @@
 #!/bin/zsh
 
 # Load sh config
-. "$HOME/.config/sh/rc"
+. "$HOME/.config/sh/env"
 
 # Set base directories
 ZSH_CONFIG="$XDG_CONFIG_HOME/zsh"
 ZSH_DATA="$XDG_DATA_HOME/zsh"
 ZSH_CACHE="$XDG_CACHE_HOME/zsh"
 if [[ ! -d "$ZSH_DATA" ]]; then
-  mkdir -p "$ZSH_DATA"
+	mkdir -p "$ZSH_DATA"
 fi
 if [[ ! -d "$ZSH_CACHE" ]]; then
-  mkdir -p "$ZSH_CACHE"
+	mkdir -p "$ZSH_CACHE"
 fi
