3108057acbc6fdafee70c30a1f7c2f0725a696f7
Eric Zhao <21zhaoe@protonmail.com>
2020-12-20 10:28:54 -0500

    pkg/zsh(t): Use zoxide instead of z.lua

--- packages/zsh/tree/.config/zsh/rc/navigation.zsh
+++ packages/zsh/tree/.config/zsh/rc/navigation.zsh
@@ -1,8 +1,12 @@
 #!/bin/zsh
 
 # z.lua
-export _ZL_DATA="$ZSH_DATA/.zlua"
-zinit light skywind3000/z.lua
+# export _ZL_DATA="$ZSH_DATA/.zlua"
+# zinit light skywind3000/z.lua
+
+# zoxide
+export _ZO_DATA_DIR="$ZSH_DATA/zoxide"
+zinit light ajeetdsouza/zoxide
 
 # autoswitch-virtualenv
 zinit light MichaelAquilina/zsh-autoswitch-virtualenv
