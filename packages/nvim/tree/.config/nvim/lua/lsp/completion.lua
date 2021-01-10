4335a7edfd528c7d59006e7387bd375734446962
Eric Zhao <21zhaoe@protonmail.com>
2021-01-09 19:45:56 -0500

    pkg/nvim(t): Disable lsp_status

--- packages/nvim/tree/.config/nvim/lua/lsp/completion.lua
+++ packages/nvim/tree/.config/nvim/lua/lsp/completion.lua
@@ -4,4 +4,13 @@ local completion = U.require('completion')
 local g = vim.g
 
 g.completion_auto_change_source = 1
+
+-- Enable snippet support
 g.completion_enable_snippet = 'snippets.nvim'
+
+-- Selection method
+g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'}
+g.completion_matching_smart_case = 1
+
+-- Timer adjustment
+g.completion_timer_cycle = 200
