32e1c8850efb55494579d21ec75d8900f7cb7b27
Eric Zhao <21zhaoe@protonmail.com>
2021-01-17 20:24:05 -0500

    pkg/nvim(t): Rust-analyzer settings and snippet remap

--- packages/nvim/tree/.config/nvim/lua/plugins/snippets.lua
+++ packages/nvim/tree/.config/nvim/lua/plugins/snippets.lua
@@ -1,15 +1,23 @@
+local U = require('util')
+
 local snippets = require 'snippets'
 local SU = require 'snippets.utils'
 
-snippets.use_suggested_mappings()
+U.map('i', '<C-l>',
+      [[ <cmd>lua return require('snippets').expand_or_advance(1)<CR> ]])
+U.map('i', '<C-j>',
+      [[ <cmd>lua return require('snippets').expand_or_advance(-1)<CR> ]])
 
 snippets.snippets = {
     _global = {todo = 'TODO: $0'},
+    -- lua snippets
     lua = {
+        -- Require statment
         req = [[local ${2:${1|S.v:match"([^.()]+)[()]*$"}} = require '$1']],
+        -- Function declaration
         func = [[function${1|vim.trim(S.v):gsub("^%S"," %0")}(${2|vim.trim(S.v)})$0 end]],
+        -- Local variable
         ["local"] = [[local ${2:${1|S.v:match"([^.()]+)[()]*$"}} = ${1}]],
-        -- Match the indentation of the current line for newlines.
         ["for"] = SU.match_indentation [[
 for ${1:i}, ${2:v} in ipairs(${3:t}) do
   $0
