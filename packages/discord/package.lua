b2341401c68e9478d943ce5dfc49380185171675
Eric Zhao <21zhaoe@protonmail.com>
2021-01-19 10:21:51 -0500

    pkg/discord(t): Add EnhancedDiscord theme

---
+++ packages/discord/package.lua
@@ -0,0 +1,26 @@
+require('lib')
+local lfs = require('lfs')
+
+pkg.name = 'discord'
+
+local edroot = '.local/share/EnhancedDiscord'
+
+local profile = require('profile').discord
+if profile.theme then
+    pkg.files.extra:extend({
+        src = 'themes/' .. profile.theme .. '.css',
+        dest = edroot .. '/plugins/style.css'
+    })
+end
+
+for plugin in lfs.dir('plugins') do
+    if plugin ~= '.' and plugin ~= '..' then
+        pkg.files.extra:extend({
+            src = 'plugins/' .. plugin,
+            dest = edroot .. '/plugins/' .. plugin
+        })
+    end
+
+end
+
+require_opt('local')
