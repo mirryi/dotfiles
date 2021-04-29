3278b7ae0ef01d39c1d6bb3e0ae3f6f8d76ff168
Eric Zhao <21zhaoe@protonmail.com>
2021-04-29 12:56:19 -0400

    pkg/nvim(t): Better gruvbox highlighting

--- packages/nvim/tree/.config/nvim/lua/themes/gruvbox-dark.lua
+++ packages/nvim/tree/.config/nvim/lua/themes/gruvbox-dark.lua
@@ -4,9 +4,18 @@ local exec = vim.api.nvim_exec
 
 -- Use gruvbox-material colorscheme
 o.background = 'dark'
-g.gruvbox_material_better_performance = 1
 g.gruvbox_material_background = 'medium'
 g.airline_theme = 'gruvbox_material'
+
+-- Enable bold function name
+g.gruvbox_material_enable_bold = 1
+
+-- Enable diagnostic highlighting
+g.gruvbox_material_diagnostic_text_highlight = 1
+g.gruvbox_material_diagnostic_line_highlight = 1
+
+-- Better performance setting
+g.gruvbox_material_better_performance = 1
 
 exec([[
 try
