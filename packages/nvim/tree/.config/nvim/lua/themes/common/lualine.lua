b1b647af1de735d0dd9028be16dd67a9ca06615f
Eric Zhao <21zhaoe@protonmail.com>
2021-05-02 17:57:39 -0400

    pkg/nvim(t): Theme-based lualine theme

---
+++ packages/nvim/tree/.config/nvim/lua/themes/common/lualine.lua
@@ -0,0 +1,16 @@
+-- luacheck: globals vim
+local lualine = require('lualine')
+
+local M = {}
+
+M.setup = function(theme)
+    lualine.setup {
+        -- TODO: Colorscheme-based theme
+        options = {theme = theme},
+        sections = {
+            lualine_c = {{'diagnostics', sources = {'nvim_lsp'}}, 'filename'}
+        }
+    }
+end
+
+return M
