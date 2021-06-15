f7f061a3ee5faca8c876a0f360c88286180d6fe2
Eric Zhao <21zhaoe@protonmail.com>
2021-06-15 13:14:56 -0400

    pkg/i3(c): Add option to have local i3config

--- packages/i3/package.lua
+++ packages/i3/package.lua
@@ -1,16 +1,23 @@
+-- luacheck: globals pkg
 local lib = require('lib')
+local lfs = require('lfs')
 
 pkg.name = 'i3'
 pkg.dependencies:extend('../battery-notify', '../polybar', '../wallpaper')
+
+local partials = {
+    common = 'config/common.i3config',
+    profile = 'config/profile.i3config.hbs'
+}
+if lfs.attributes('config/local.i3config.hbs') ~= nil then
+    partials.lcl = 'config/local.i3config.hbs'
+end
 
 pkg.files.templates:push({
     src = 'config/config.i3config.hbs',
     dest = '.config/i3/config',
     engine = 'handlebars',
-    partials = {
-        common = 'config/common.i3config',
-        profile = 'config/profile.i3config.hbs'
-    }
+    partials = partials
 })
 
 pkg.hooks.post.push({name = 'Reload i3', command = 'hooks/reload-i3.sh'})
