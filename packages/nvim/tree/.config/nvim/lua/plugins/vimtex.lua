764376d38161ba4ca40a9eb87bfa8c2015350b57
Eric Zhao <21zhaoe@protonmail.com>
2021-07-12 15:07:55 -0400

    pkg/nvim(t): More Rust snippets

--- packages/nvim/tree/.config/nvim/lua/plugins/vimtex.lua
+++ packages/nvim/tree/.config/nvim/lua/plugins/vimtex.lua
@@ -1,5 +1,7 @@
 -- luacheck: globals vim
 local g = vim.g
+local bind = require 'util.bind'
+
 g.vimtex_enabled = 1
 
 -- Syntax options
@@ -7,3 +9,5 @@ g.vimtex_syntax_enabled = 1
 
 -- View options
 g.vimtex_view_method = 'zathura'
+
+bind.nmap('gC', '<cmd>VimtexContextMenu<CR>', {noremap = true})
