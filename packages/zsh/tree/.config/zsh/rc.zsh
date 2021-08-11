9b03793088222f844c06ddf42cc857a6153c1b73
Eric Zhao <21zhaoe@protonmail.com>
2021-08-11 17:10:30 -0400

    pkg(t): Various shell/terminal tweaks

--- packages/zsh/tree/.config/zsh/rc.zsh
+++ packages/zsh/tree/.config/zsh/rc.zsh
@@ -5,7 +5,6 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-
 local HERE="$(dirname "${(%):-%N}")"
 local CORE="$HERE/rc"
 
--- packages/zsh/tree/.config/zsh/rc/completion.zsh
+++ packages/zsh/tree/.config/zsh/rc/completion.zsh
@@ -4,52 +4,39 @@
 zinit wait'2' lucid light-mode for \
   hlissner/zsh-autopair
 
-# fzf tab
-zinit wait'0' lucid light-mode for \
-  Aloxaf/fzf-tab
-
-# OMZ git plugin
+# OMZ git
 zinit wait'1' lucid has'git' light-mode for \
   OMZ::lib/git.zsh \
   OMZ::plugins/git/git.plugin.zsh
 
-# syntax highlighting, autosuggestions, completions plugins
+# autosuggestions, more completions
 zinit wait lucid light-mode for \
   atload'!_zsh_autosuggest_start' zsh-users/zsh-autosuggestions \
   blockf zsh-users/zsh-completions
 
-zinit wait'0' lucid light-mode for \
+zinit wait lucid light-mode for \
+  gentoo/gentoo-zsh-completions
+
+# syntax highlighting
+zinit light-mode for \
   atinit'ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay' zdharma/fast-syntax-highlighting
+
+# autocomplete
+zinit light-mode for \
+  marlonrichert/zsh-autocomplete
+zstyle ':autocomplete:*' list-lines 10
+zstyle ':autocomplete:*' recent-dirs zoxide
+zstyle ':autocomplete:*' widget-style menu-complete
 
 if [[ -d "${ZSH_DATA}/completions" ]]; then
   zinit creinstall -Q "${ZSH_DATA}/completions"
 fi
 
-# disable sort when completing options of any command
-zstyle ':completion:complete:*:options' sort false
-
-# pass default opts to fzf-tab
-zstyle ':fzf-tab:complete:*' fzf-flags ${=FZF_DEFAULT_OPTS}
-
-# use input as query string when completing zlua
-zstyle ':fzf-tab:complete:_zlua:*' query-string input
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
 # preview of commandline arguments when completing `kill`
 zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm,cmd -w -w"
-zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-preview \
-  'ps --pid=$word -o cmd --no-headers -w -w'
-zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-flags --preview-window=down:3:wrap
 
-# preview of directory by exa when completing cd
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -a -1 --color=always $realpath'
-
-# case-insensitive completion
+# misc completion menu settings
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|=*' 'l:|=* r:|=*'
-
-zstyle ':completion:*:*:kill:*' menu yes select
-zstyle ':completion:*:kill:*'   force-list always
-zstyle ':completion:*:*:killall:*' menu yes select
-zstyle ':completion:*:killall:*'   force-list always
-
 zstyle ':completion:*:*:*:default' menu yes select search
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
