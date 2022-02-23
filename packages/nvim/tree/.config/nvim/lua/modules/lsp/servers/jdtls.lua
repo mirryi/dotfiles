ced5255dc3d0c3b044a6827c534e6319147918a7
Eric Zhao <21zhaoe@protonmail.com>
2022-02-23 18:04:11 -0500

    pkg/nvim(t): Various cleanup/lsp/snippets

--- packages/nvim/tree/.config/nvim/lua/modules/lsp/servers/jdtls.lua
+++ packages/nvim/tree/.config/nvim/lua/modules/lsp/servers/jdtls.lua
@@ -1,11 +1,10 @@
 -- luacheck: globals vim
 local jdtls = require('jdtls')
 
-local paths = require('core.paths')
 local handlers = require('modules.lsp.handlers')
 
 jdtls.start_or_attach({
-	cmd = { paths.config() .. '/lua/modules/lsp/servers/jdtls.sh' },
+	cmd = { vim.fn.stdpath('config') .. '/lua/modules/lsp/servers/jdtls.sh' },
 	on_attach = handlers.on_attach,
 	capabilities = handlers.capabilities,
 })
