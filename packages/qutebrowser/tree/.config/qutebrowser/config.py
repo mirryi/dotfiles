d0b0a4995df872db36bb051500f74ccc1ad6b6ec
Eric Zhao <21zhaoe@protonmail.com>
2022-04-09 14:57:47 -0400

    pkg/qutebrowser(t): Update custom headers for privacy

--- packages/qutebrowser/tree/.config/qutebrowser/config.py
+++ packages/qutebrowser/tree/.config/qutebrowser/config.py
@@ -66,10 +66,18 @@ c.content.webgl = False
 c.content.javascript.can_access_clipboard = False
 # disable geolocation
 c.content.geolocation = False
+# set common accept header
+c.content.headers.accept_language = 'en-US,en;q=0.5'
+c.content.headers.custom = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+}
+
 # set user agent string
-c.content.headers.user_agent = ('Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-                                ' AppleWebKit/537.36 (KHTML, like Gecko)'
-                                ' Chrome/79.0.3945.117 Safari/537.36')
+c.content.headers.user_agent = (
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+    'AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/97.0.4692.99 Safari/537.36'
+)
 # enable pdfjs
 c.content.pdfjs = True
 # set webrtc policy
@@ -132,8 +140,8 @@ if JMATRIX_LOADED not in os.environ:
     except ValueError:
         pass
 
-# enable dynamic theme loading
-config.source("qutewal.py")
+# load theme
+config.source('qutewal.py')
 
 # load local config
 local_config_file = "config.local.py"
