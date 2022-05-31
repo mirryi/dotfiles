6942df3481714c32ee07c4461952b1f1b41e8057
Eric Zhao <21zhaoe@protonmail.com>
2022-05-31 13:43:29 -0400

    pkg/firefox: Add local bookmarks file

--- packages/firefox/package.lua
+++ packages/firefox/package.lua
@@ -1,6 +1,14 @@
 local lib = require('lib')
+local lfs = require('lfs')
 
 pkg.name = 'firefox'
+
+if lfs.attributes('local/bookmarks') then
+    pkg.files.extra:extend {
+        src = 'local/bookmarks',
+        dest = '.config/tridactyl/bookmarks',
+    }
+end
 
 -- Load local file if it exists
 lib.require_opt('local')
