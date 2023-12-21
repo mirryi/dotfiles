6fc21a4a6f7243bf737b23803405c36e48a5f879
Eric Zhao <21zhaoe@protonmail.com>
2023-12-21 15:43:40 -0500

    archive: Remove all

--- archive/mutt/package.lua
+++
@@ -1,7 +0,0 @@
-local lib = require('lib')
-
-pkg.name = 'mutt'
-pkg.dependencies:extend('../sh')
-
--- Load local file if exists
-lib.require_opt('local')
--- archive/networkmanager-dmenu/package.lua
+++
@@ -1,6 +0,0 @@
-local lib = require('lib')
-
-pkg.name = 'networkmanager-dmenu'
-
--- Load local file if exists
-lib.require_opt('local')
