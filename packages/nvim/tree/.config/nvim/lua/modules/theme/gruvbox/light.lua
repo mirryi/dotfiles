9963093519de59a931704f571fb7191b3be8d6e6
Eric Zhao <21zhaoe@protonmail.com>
2023-12-21 15:51:45 -0500

    pkg/nvim(t): Remove gruvbox light theme

--- packages/nvim/tree/.config/nvim/lua/modules/theme/gruvbox/light.lua
+++
@@ -1,9 +0,0 @@
--- luacheck: globals vim
--- Initialize colorscheme
-local shared = require('modules.theme.gruvbox.shared')
-shared.colorscheme('light', 'original', 'medium')
-
--- Initialize lualine
-local lualine = require('modules.theme.common.lualine')
--- TODO: light theme
-lualine.setup('gruvbox-material')
