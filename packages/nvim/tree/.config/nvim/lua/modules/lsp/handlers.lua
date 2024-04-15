70aec0a17bc9803dda27311bca37f929d73198f2
Eric Zhao <21zhaoe@protonmail.com>
2024-04-15 11:57:44 -0400

    pkg/nvim(t): Disable lsputil handlers

--- packages/nvim/tree/.config/nvim/lua/modules/lsp/handlers.lua
+++ packages/nvim/tree/.config/nvim/lua/modules/lsp/handlers.lua
@@ -13,13 +13,13 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagn
 
 -- Use lsputil handlers
 -- vim.lsp.handlers['textDocument/codeAction'] = require('lsputil.locations').code_action_handler
-vim.lsp.handlers['textDocument/references'] = require('lsputil.locations').references_handler
-vim.lsp.handlers['textDocument/definition'] = require('lsputil.locations').definition_handler
-vim.lsp.handlers['textDocument/declaration'] = require('lsputil.locations').declaration_handler
-vim.lsp.handlers['textDocument/typeDefinition'] = require('lsputil.locations').typeDefinition_handler
-vim.lsp.handlers['textDocument/implementation'] = require('lsputil.locations').implementation_handler
-vim.lsp.handlers['textDocument/documentSymbol'] = require('lsputil.symbols').document_handler
-vim.lsp.handlers['workspace/symbol'] = require('lsputil.symbols').workspace_handler
+-- vim.lsp.handlers['textDocument/references'] = require('lsputil.locations').references_handler
+-- vim.lsp.handlers['textDocument/definition'] = require('lsputil.locations').definition_handler
+-- vim.lsp.handlers['textDocument/declaration'] = require('lsputil.locations').declaration_handler
+-- vim.lsp.handlers['textDocument/typeDefinition'] = require('lsputil.locations').typeDefinition_handler
+-- vim.lsp.handlers['textDocument/implementation'] = require('lsputil.locations').implementation_handler
+-- vim.lsp.handlers['textDocument/documentSymbol'] = require('lsputil.symbols').document_handler
+-- vim.lsp.handlers['workspace/symbol'] = require('lsputil.symbols').workspace_handler
 
 -- Grouped on_attach
 M.on_attach = function(client, bufnr)
