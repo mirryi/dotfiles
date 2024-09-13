1f7eec8c238f23caec3dbd9ac45fd62df3435c6d
Eric Zhao <21zhaoe@protonmail.com>
2024-09-13 18:27:07 -0400

    pkg/mail: now use thunderbird

--- packages/mail/tree/.config/neomutt/scripts/mbsync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -z "${TMUX}" ]; then
-	mbsync -c "${XDG_CONFIG_HOME}/isync/mbsyncrc" -a
-else
-	tmux new-window -d -n "mailfetch" mbsync -c "${XDG_CONFIG_HOME}/isync/mbsyncrc" -a
-fi
