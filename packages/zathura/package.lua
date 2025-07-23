ed99b4b8f994e264333f6beeb6a4139c4c973f93
Eric Zhao <21zhaoe@protonmail.com>
2025-07-23 16:37:19 -0400

    pkg/zathura(t): add gruvbox themes

--- packages/zathura/package.lua
+++ packages/zathura/package.lua
@@ -1,7 +1,13 @@
 local lib = require('lib')
+local profile = require('profile').zathura
 
 pkg.name = 'zathura'
 pkg.dependencies:extend('../sh')
+
+pkg.files.extra:push {
+    src = 'themes/' .. profile.theme,
+    dest = '.config/zathura/theme',
+}
 
 -- Load local file if it exists
 lib.require_opt('local')
