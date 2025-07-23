dd9b86b22316a5b3021cfe5d67962853db012904
Eric Zhao <21zhaoe@protonmail.com>
2025-07-23 16:51:10 -0400

    pkg/igrep(t): set theme depending on profile

--- packages/igrep/package.lua
+++ packages/igrep/package.lua
@@ -3,5 +3,15 @@ local lib = require('lib')
 pkg.name = 'igrep'
 pkg.dependencies:extend('../sh')
 
+pkg.files.trees:clear()
+pkg.files.templates:push {
+    src = 'rc/rc.hbs',
+    dest = '.config/sh/load/igrep/rc',
+    engine = 'handlebars',
+}
+
+local profile = require('profile').igrep
+pkg.variables:overwrite(profile)
+
 -- Load local file if exists
 lib.require_opt('local')
