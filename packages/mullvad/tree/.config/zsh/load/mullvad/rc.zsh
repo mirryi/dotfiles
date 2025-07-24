189cf62d17add3e77daf5f10a5b7d295566db9f5
Eric Zhao <21zhaoe@protonmail.com>
2025-07-24 10:47:28 -0400

    pkg/mullvad(t): generate completions once only

--- packages/mullvad/tree/.config/zsh/load/mullvad/rc.zsh
+++ packages/mullvad/tree/.config/zsh/load/mullvad/rc.zsh
@@ -1,4 +1,6 @@
 #!/bin/zsh
 if (( $+commands[mullvad] )); then
-  mullvad shell-completions zsh "$XDG_DATA_HOME/zsh/"
+  if [[ ! -f "$XDG_DATA_HOME/zsh/_mullvad" ]]; then
+    mullvad shell-completions zsh "$XDG_DATA_HOME/zsh/"
+  fi
 fi
