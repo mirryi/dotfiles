2cb1eef817d248be5a28b34d9edb83a8f14a43fe
Eric Zhao <21zhaoe@protonmail.com>
2026-04-13 10:34:09 -0400

    pkg/nvim(t): use 2 tab width for typescript

--- packages/nvim/tree/.config/nvim/after/ftplugin/typescriptreact.lua
+++ packages/nvim/tree/.config/nvim/after/ftplugin/typescriptreact.lua
@@ -1,6 +1,5 @@
--- luacheck: globals vim
-local bo = vim.bo
+-- local bo = vim.bo
 
-bo.tabstop = 4
-bo.softtabstop = 4
-bo.shiftwidth = 4
+-- bo.tabstop = 4
+-- bo.softtabstop = 4
+-- bo.shiftwidth = 4
