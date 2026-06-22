b7312e1ec2e18a63837cf0f986138feaa5ee8d20
Eric Zhao <21zhaoe@protonmail.com>
2026-06-22 15:40:12 -0400

    pkg/eww(t): use eww profile section

--- packages/eww/package.lua
+++ packages/eww/package.lua
@@ -4,11 +4,11 @@ pkg.name = 'eww'
 pkg.dependencies:extend('../sh')
 
 pkg.files.trees:front().ignore:extend('**/*.hbs')
-pkg.files.templates:extend({
+pkg.files.templates:extend {
     src = 'tree/.config/eww/eww.scss.hbs',
     dest = '.config/eww/eww.scss',
     engine = 'handlebars',
-})
+}
 
 local home = os.getenv('HOME')
 pkg.hooks.post:extend {
@@ -16,9 +16,9 @@ pkg.hooks.post:extend {
     command = home .. '/.local/bin/launch-eww',
 }
 
--- Reuse the polybar profile palette/font; both gruvbox profiles already
--- define it, so there is one source of truth for the theme.
-lib.use_profile('polybar')
+-- Palette/font come from the eww profile section, so the theme has one
+-- source of truth.
+lib.use_profile('eww')
 
 -- Load local file if it exists
 lib.require_opt('local')
