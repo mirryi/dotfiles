e624285ea72d315126dd52330708b0625a532ccc
Eric Zhao <21zhaoe@protonmail.com>
2026-08-06 18:13:31 -0400

    pkg/nvim(t): close buffer after eunuch :Remove

--- packages/nvim/tree/.config/nvim/lua/plugins/tools.lua
+++ packages/nvim/tree/.config/nvim/lua/plugins/tools.lua
@@ -13,5 +13,16 @@ return {
     {
         -- unix commands
         'tpope/vim-eunuch',
+        init = function()
+            -- close orphaned buffer after :Remove
+            vim.api.nvim_create_autocmd('User', {
+                pattern = 'FileUnlinkPost',
+                callback = function()
+                    if #vim.fn.getbufinfo { buflisted = 1 } > 1 then
+                        vim.cmd('bdelete!')
+                    end
+                end,
+            })
+        end,
     },
 }
