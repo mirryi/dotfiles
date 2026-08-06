d40c8e0b88d3a123a455c637930d6ef24f28790b
Eric Zhao <21zhaoe@protonmail.com>
2026-08-06 17:11:44 -0400

    pkg/yazi(t): show btime/mtime in status bar

--- packages/yazi/tree/.config/yazi/init.lua
+++ packages/yazi/tree/.config/yazi/init.lua
@@ -6,3 +6,23 @@ require('yamb'):setup {
     jump_notify = true,
     cli = 'fzf',
 }
+
+-- btime/mtime of hovered file
+Status:children_add(function(self)
+    local h = self._current.hovered
+    if not h then
+        return ''
+    end
+
+    local function fmt(t)
+        return t and os.date('%Y-%m-%d %H:%M', math.floor(t)) or '-'
+    end
+
+    return ui.Line {
+        ui.Span('C '):fg('cyan'),
+        ui.Span(fmt(h.cha.btime)),
+        ui.Span(' M '):fg('yellow'),
+        ui.Span(fmt(h.cha.mtime)),
+        ui.Span(' '),
+    }
+end, 500, Status.RIGHT)
