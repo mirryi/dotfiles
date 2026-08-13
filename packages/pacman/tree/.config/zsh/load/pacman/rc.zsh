3302f5d494dc2ebfcb50fd8dd901d82f3cfcd1e8
Eric Zhao <21zhaoe@protonmail.com>
2026-08-13 16:03:57 -0400

    pkg/pacman(t): suggest packages for unknown commands with pkgfile

--- packages/pacman/tree/.config/zsh/load/pacman/rc.zsh
+++ packages/pacman/tree/.config/zsh/load/pacman/rc.zsh
@@ -1,4 +1,8 @@
 #!/bin/zsh
+# suggest packages providing an unknown command
+[[ ! -f /usr/share/doc/pkgfile/command-not-found.zsh ]] ||
+	. /usr/share/doc/pkgfile/command-not-found.zsh
+
 # pacman completion settings
 zstyle ':completion:*:pacman:*' force-list always
 zstyle ':completion:*:*:pacman:*' menu yes select
