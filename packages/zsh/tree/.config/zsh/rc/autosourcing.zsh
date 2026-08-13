2ce3ceaaa18a5f6f0d674edd28035823218344cb
Eric Zhao <21zhaoe@protonmail.com>
2026-08-13 16:04:22 -0400

    pkg/zsh(t): remove unused command-not-found, autoenv, and poetry plugins

--- packages/zsh/tree/.config/zsh/rc/autosourcing.zsh
+++ packages/zsh/tree/.config/zsh/rc/autosourcing.zsh
@@ -5,7 +5,3 @@ eval "$(direnv hook zsh)"
 
 # MichaelAquilina/zsh-autoswitch-virtualenv
 export AUTOSWITCH_VIRTUAL_ENV_DIR="${XDG_DATA_HOME}/venvs"
-
-# Tarrasch/zsh-autoenv
-export AUTOENV_FILE_ENTER=".autoenv.zsh"
-export AUTOENV_FILE_LEAVE=".autoenv.exit.zsh"
