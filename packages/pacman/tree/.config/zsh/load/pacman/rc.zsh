8ca74328c47a8d2e0e3c50d7a0952717d35ac6b2
Eric Zhao <21zhaoe@protonmail.com>
2026-08-13 16:49:10 -0400

    pkg/pacman(t): let fzf-tab drive pacman completion

--- packages/pacman/tree/.config/zsh/load/pacman/rc.zsh
+++ packages/pacman/tree/.config/zsh/load/pacman/rc.zsh
@@ -4,6 +4,4 @@
 	. /usr/share/doc/pkgfile/command-not-found.zsh
 
 # pacman completion settings
-zstyle ':completion:*:pacman:*' force-list always
-zstyle ':completion:*:*:pacman:*' menu yes select
 zstyle ':fzf-tab:complete:pacman:*' fzf-preview 'pacman -Qi $realpath'
