4a565254b1f59214962c3d868f5ad9917c97a124
Eric Zhao <21zhaoe@protonmail.com>
2026-08-13 17:13:02 -0400

    pkg/pacman(t): remove fzf-tab preview style

--- packages/pacman/tree/.config/zsh/load/pacman/rc.zsh
+++ packages/pacman/tree/.config/zsh/load/pacman/rc.zsh
@@ -6,4 +6,3 @@
 # pacman completion settings
 zstyle ':completion:*:pacman:*' force-list always
 zstyle ':completion:*:*:pacman:*' menu yes select
-zstyle ':fzf-tab:complete:pacman:*' fzf-preview 'pacman -Qi $realpath'
