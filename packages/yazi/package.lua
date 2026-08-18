53de3537f713d8fdfcaf7be012da866403490d60
Eric Zhao <21zhaoe@protonmail.com>
2026-08-18 16:46:36 -0400

    pkg/yazi(t): mimic ranger's rifle.conf

--- packages/yazi/package.lua
+++ packages/yazi/package.lua
@@ -9,6 +9,16 @@ pkg.files.templates:push {
     engine = 'handlebars',
 }
 
+pkg.files.templates:push {
+    src = 'config/yazi.toml.hbs',
+    dest = '.config/yazi/yazi.toml',
+    engine = 'handlebars',
+    partials = {
+        common = 'config/common.toml',
+        open = 'config/open.toml',
+    },
+}
+
 pkg.hooks.post:push { name = 'Install yazi plugins', command = 'hooks/install-plugins.sh' }
 
 lib.use_profile('yazi')
